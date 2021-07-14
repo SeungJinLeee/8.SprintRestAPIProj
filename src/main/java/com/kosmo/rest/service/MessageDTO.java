80f5617928d44f085785e8646a7ddab2c8042b46
SeungJinLeee <82311332+SeungJinLeee@users.noreply.github.com>
2021-07-14 23:55:04 +0900

    Add files via upload

---
+++ src/main/java/com/kosmo/rest/service/MessageDTO.java
@@ -0,0 +1,11 @@
+package com.kosmo.rest.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MessageDTO {
+	private String dataTitle;
+	private String dataBody;
+}
